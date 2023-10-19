050fe250a37e11eea6d59dfb985ed26ef6f2078b
Mireia Parra <miahigurashi02@gmail.com>
2023-10-19 20:01:15 +0200

    Initial commit

---
+++ src/main/java/com/libros/reserva/ReservaDeLibrosApplication.java
@@ -0,0 +1,13 @@
+package com.libros.reserva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservaDeLibrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservaDeLibrosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/libros/reserva/ReservaDeLibrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.libros.reserva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservaDeLibrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
